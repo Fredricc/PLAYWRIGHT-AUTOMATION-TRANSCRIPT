d25e12b0e2b281c529261d629b0db07f6c57330d
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-10-18 15:53:34 +0300

    Playwright css selector and text selector implementation

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -7,8 +7,36 @@ test.describe('Home', () => {
     await page.goto('https://www.google.com/');
     
     //verify title
-    await expect(page).toHaveTitle('Google');
-    
-    })
-    
+    await expect(page).toHaveTitle('Google');   
+
+    })  
 })
+
+test.describe('Get Started', () => {
+    test('Click get started button using text selector', async ({ page }) => {
+        //open URL
+        await page.goto('https://playwright.dev/');
+
+        //Click the button 
+        await page.locator('text=Get started').click();
+
+
+        //verify url has docs/intro 
+        await expect(page).toHaveURL('https://playwright.dev/docs/intro');
+        
+    })
+
+    test('Verify heading text is visible using text selector', async ({ page }) => {
+        //open URL
+        await page.goto('https://playwright.dev/');
+
+        //find the text locator
+        const headingText = await page.locator('text= Playwright enables reliable end-to-end testing for modern web apps.');
+
+
+        //verify heading text is visible
+        await expect(headingText).toBeVisible();
+        
+    })
+})
+
